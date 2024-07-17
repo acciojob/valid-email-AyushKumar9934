7a97ecd2200289a605d361c2c2da5fe7ac4e09bd
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 15:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function validEmail(str) {
+
   //your JS code here.
-}
+	const validEmail = (email) => {
+    
+   if(  String(email)
+    
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )==null){ return false;}
+    return true;
+
+      
+  };
+
+
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+ const str = prompt("Enter an email address.");
 alert(validEmail(str));
